df92634493068e823f21526dc84d27bfe9328617
Stephan Kolontay <stepan.kolontaj@gmail.com>
2023-05-16 00:18:00 +0300

    added json handler (to_test)

---
+++ artificial_pancreas/json_handler.m
@@ -0,0 +1,14 @@
+function json_handler(data)
+    patient.BGL = data.BGL; % blood glucose level
+    patient.BGR = data.BGR; % blood glucose rate
+    patient.AVG = data.AVG; % average blood glucose level
+    patient.SD = data.SD; % standard deviation 
+    patient.GMI = data.GMI; % glucose management indicator
+    patient.TIR.high = data.TIR.high; % time in range above norm
+    patient.TIR.inRange = data.TIR.inRange; % time in range witihin norm
+    patient.TIR.low = data.TIR.low; % time in range below norm
+
+    patient_chn = 'http://localhost:8080/patient';
+
+    webwrite(patient_chn, patient);
+end
