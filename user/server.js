043e2ca018cf18ecb3584accb076206a6d771731
stfn_ko <stepan.kolontaj@gmail.com>
2023-05-16 23:42:24 +0100

    added cors and get to api

--- user/server.js
+++ user/server.js
@@ -1,26 +1,23 @@
 const express = require('express');
-const fs = require('fs');
+const cors = require('cors');
 
 const app = express();
 const port = 8080;
 
 app.use(express.json());
+app.use(cors()); 
+
+let jsonData = {};
+
+
+app.get('/patient', (req, res) => {
+  res.json(jsonData);
+});
+
 
 app.post('/patient', (req, res) => {
-  const jsonData = req.body;
-
-  const filePath = `./user.json`;
-  
-  // Write the file
-  fs.writeFile(filePath, JSON.stringify(jsonData), (err) => {
-    if (err) {
-      console.error(`Error writing to file ${filePath}: ${err}`);
-      res.status(500).send('Error writing file');
-    } else {
-      console.log(`JSON data written to file ${filePath}`);
-      res.send('JSON data received and processed');
-    }
-  });
+  jsonData = req.body;
+  res.send('JSON data received and processed');
 });
 
 
