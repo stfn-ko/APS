978fece97f73041e6aa866463beba6d84d2e4648
stfn_ko <stepan.kolontaj@gmail.com>
2023-05-16 23:23:24 +0100

    added time stamps to the frontend

--- user/app/main.js
+++ user/app/main.js
@@ -39,7 +39,7 @@ async function fetchData() {
     }
 
     if (data.TimeStamp) {
-      timeStamp.textContent = `${data.TimeStamp} - ${data.TimeStamp}`;
+      timeStamp.textContent = `${data.TimeStamp.initial}  -  ${data.TimeStamp.current}`;
     } else {
       timeStamp.textContent = '--/--/--';
     }
